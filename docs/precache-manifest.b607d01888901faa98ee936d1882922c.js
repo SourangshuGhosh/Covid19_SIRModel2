3f4db16afe175f597c41b6ce0568daa781391fbe
Sourangshu Ghosh <sourangshug123@gmail.com>
2020-07-20 00:39:41 +0530

    docs file uploaded

---
+++ docs/precache-manifest.b607d01888901faa98ee936d1882922c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92bdba5e98541eafd142f23d8546892e",
+    "url": "/coronavirus-simulation/index.html"
+  },
+  {
+    "revision": "ca9ec50f8dd8d6659fb4",
+    "url": "/coronavirus-simulation/static/css/main.24ccaaa2.chunk.css"
+  },
+  {
+    "revision": "d03397fb1d226a624a97",
+    "url": "/coronavirus-simulation/static/js/2.2695d89b.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/coronavirus-simulation/static/js/2.2695d89b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca9ec50f8dd8d6659fb4",
+    "url": "/coronavirus-simulation/static/js/main.041b4d7e.chunk.js"
+  },
+  {
+    "revision": "77e2921deadc088d248c",
+    "url": "/coronavirus-simulation/static/js/runtime-main.43b6624d.js"
+  }
+]);
